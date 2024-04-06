feebfeaaede688b5f870dec7c03e6471e80e42ce
emoninit <emongacc@gmail.com>
2024-04-07 03:48:19 +0600

    completed 2nd section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
         jost: ["Jost"],
         workSans: ["Work Sans"],
       },
+      height: {
+        xl: "36rem",
+        "2xl": "42rem",
+        "3xl": "48rem",
+        "4xl": "56rem",
+        "5xl": "64rem",
+        "6xl": "72rem",
+        "7xl": "80rem",
+      },
     },
   },
+
   plugins: [],
 };
