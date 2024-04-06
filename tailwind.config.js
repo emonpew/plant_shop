4e4e867ce8647902477b547cf8f3cc520e16500c
emoninit <emongacc@gmail.com>
2024-04-06 15:15:47 +0600

    Finished Home page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jost: ["Jost"],
+        workSans: ["Work Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
