00fc4d3b6bc5b981dc1061f6b33a5a8022cc15c8
emoninit <emongacc@gmail.com>
2024-04-07 16:54:25 +0600

    added upto care for plant section

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,18 @@ module.exports = {
         "6xl": "72rem",
         "7xl": "80rem",
       },
+      minHeight: {
+        xl: "36rem",
+        "2xl": "42rem",
+        "3xl": "48rem",
+        "4xl": "56rem",
+        "5xl": "64rem",
+        "6xl": "72rem",
+        "7xl": "80rem",
+      },
+      colors: {
+        sky: "#C1D0E4",
+      },
     },
   },
 
